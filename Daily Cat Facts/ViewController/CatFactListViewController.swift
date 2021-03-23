242cafa2beabcc4dde147f34a92e66f646b6f094
Adriaan <adriaan9@hotmail.co.za>
2021-03-23 13:41:10 +0200

    revert

--- Daily Cat Facts/ViewController/CatFactListViewController.swift
+++ Daily Cat Facts/ViewController/CatFactListViewController.swift
@@ -15,7 +15,6 @@ class CatFactListViewController: UITableViewController {
     private let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
     private var catFactManager = CatFactManager()
     
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         catFactManager.delegate = self
